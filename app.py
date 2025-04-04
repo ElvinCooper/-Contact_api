8d034f806cac051853084fcf5d008d2b34d7b164
Elvin Cooper <ing.elvin01cooper@gmail.com>
2025-04-04 17:07:02 -0400

    CRUD completo

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sqlite3, requests
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from flask_marshmallow import Marshmallow
+from marshmallow.exceptions import ValidationError
+
 
 
 app = Flask(__name__)
@@ -26,7 +28,9 @@ class Contacto(db.Model):
 class ContactoSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Contacto
-        load_instance = True
+        #load_instance = True
+        nombre = ma.Str(required=True, validate=lambda n: 1 <= len(n) <= 80)  # Cambiar "name" por "nombre"
+        email = ma.Email(required=True)
 
 
 @app.cli.command('db_create')
@@ -89,15 +93,13 @@ def get_contacto(id):
 @app.route('/insertar/<string:nombre>/<string:email>', methods=['POST'])
 def put_contact(nombre: str, email: str): 
     data = request.get_json(silent=True)
-    # nombre = request.json.get(nombre)
-    # email  = request.json.get(email) 
 
     if not nombre or not email:
         return jsonify({"Mensaje": "Faltan parametros (nombre y/o email)"}), 400
     
     # Validar formato de email
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            return jsonify({"Error": "Formato de email inválido"}), 400
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return jsonify({"Error": "Formato de email inválido"}), 400
 
     
     #email = data.get("email")
@@ -113,7 +115,46 @@ def put_contact(nombre: str, email: str):
     contacto_schema = ContactoSchema()
     return jsonify(contacto_schema.dump(nuevo_contacto)), 201
     
-    
+
+# Endpoint para actualizar un recurso en la bd
+@app.route('/update/<int:id>', methods=['PUT'])   
+def update_contact(id):
+    contacto = Contacto.query.get_or_404(id)
+
+    try:
+        data = ContactoSchema().load(request.get_json(), partial=True)
+        if 'nombre' in data:
+            contacto.nombre = data['nombre']
+        if 'email' in data:
+        
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", data['email']):
+                return jsonify({"Error": "Formato de email inválido"}), 400
+
+            contacto.email = data['email']   
+
+        db.session.commit()
+        contacto_schema = ContactoSchema()
+        return jsonify(contacto_schema.dump(contacto))
+    except ValidationError as e:
+        return jsonify({"Error": e.messages}), 400
+    except Exception as err:
+        return jsonify({"Error": str(err)}), 400
+                
+
+
+# ENDPOINT PARA ELIMINAR UN RECURSO DE LA BD
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_contact(id):
+    contacto = Contacto.query.get_or_404(id)
+
+    db.session.delete(contacto)
+    db.session.commit()
+    return jsonify({"mensaje": f"Registro '{contacto.nombre}' eliminado exitosamente"}), 200
+
+
+
+
+
 
 
 if __name__ == '__main__':
